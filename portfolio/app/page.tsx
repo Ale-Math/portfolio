ac674b02a4dd0bc7e2381e89fa384ad01d22c9af
Alexander Mathew <alexandermathew27@gmail.com>
2025-07-16 17:20:52 +0530

    added image

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -16,16 +16,30 @@ export default function Home() {
           </ul>
         </div>
       </div>
-      <div className="py-24">
-        <div></div>
-        <div>
-          Alexander P.
-          <br />
-          Mathew
-          <HandSvg />
+      <div className="py-24 w-11/12 flex justify-center">
+        <div className="w-1/5"></div>
+        <div className="w-2/5 space-y-4">
+          <p className="text-6xl font-bold">Alexander P.</p>
+          <div className="flex items-center">
+            <p className="text-6xl font-bold">Mathew</p> &nbsp;&nbsp;{" "}
+            <HandSvg />
+          </div>
+          <p className="text-xl text-gray-600">
+            ———— &nbsp; Software Developer
+          </p>
+          <p className="w-4/5 text-lg/6 text-gray-500">
+            As a Software Engineer, I am driven by a deep passion for acquiring
+            new skills and expertise to create stunning and fully responsive
+            applications.
+          </p>
         </div>
-
-        <div></div>
+        <div className="w-2/5"></div>
+      </div>
+      <div>
+        <div>
+          <p>About Me</p>
+          <p>My Introduction</p>
+        </div>
       </div>
     </div>
   );
