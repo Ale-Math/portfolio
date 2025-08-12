9984f803bf17f519cd947cc81791e1a329a52b1a
Alexander Mathew <alexandermathew27@gmail.com>
2025-08-12 18:03:29 +0530

    added padding to service tile

--- portfolio/app/Components/serviceTile.tsx
+++ portfolio/app/Components/serviceTile.tsx
@@ -8,10 +8,13 @@ interface ServicesTileProps {
 
 export function ServicesTile(props: ServicesTileProps) {
   return (
-    <div className="w-60 p-5 pr-10 py-20 flex flex-col items-start space-y-3 shadow border-gray-300 border bg-slate-50">
+    <div className="w-64 p-5 pr-14 py-20 flex flex-col items-start space-y-5 shadow border-gray-300 border bg-slate-50">
       <div className="p-2">{props.icon}</div>
       <p className="text-xl">{props.title}</p>
-      <p className="text-md text-gray-500">view more {props.icon2}</p>
+      <div className="flex">
+        <p className="text-md text-gray-500">view more </p>
+        {props.icon2}
+      </div>
     </div>
   );
 }
