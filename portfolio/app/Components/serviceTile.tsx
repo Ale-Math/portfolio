5d8462817d112e946314a83aede5bfbe61f0dc3c
Alexander Mathew <alexandermathew27@gmail.com>
2025-08-12 17:59:13 +0530

    updated service card

--- portfolio/app/Components/serviceTile.tsx
+++ portfolio/app/Components/serviceTile.tsx
@@ -7,10 +7,10 @@ interface ServicesTileProps {
 
 export function ServicesTile(props: ServicesTileProps) {
   return (
-    <div className="w-60 p-4 flex flex-col items-start space-y-3 shadow border-gray-300 border bg-slate-50">
+    <div className="w-60 p-5 pr-10 py-20 flex flex-col items-start space-y-3 shadow border-gray-300 border bg-slate-50">
       <div className="p-2">{props.icon}</div>
-      <p className="text-lg font-semibold">{props.title}</p>
-      <p className="text-sm text-gray-500">view more -</p>
+      <p className="text-xl">{props.title}</p>
+      <p className="text-md text-gray-500">view more -</p>
     </div>
   );
 }
